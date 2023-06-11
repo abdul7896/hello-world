e86991c672b78b07a0a53c115928d1cf6f4919b8
Abdul Syed <syed_abdul_basit@hotmail.com>
2023-06-11 21:53:49 +0100

    Update HelloWorldTest.java

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -7,12 +7,6 @@ public class HelloWorldTest {
     public void testPrintHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         String actual = helloWorld.getMessage();
-        String expected = "Hello World!";
-                        
-         if (expected.equals(actual)) {
-            System.out.println("Test passed!");
-        } else {
-            System.out.println("Test failed!");
-        }
+        assertEquals("Hello World!", output);
     }
 }
